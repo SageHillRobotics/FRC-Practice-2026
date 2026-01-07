e33e8dfff52b0b0f7c217768056557984d4f5bdb
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2026-01-07 14:56:26 -0800

    finished possible implementation of practice chassis

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,8 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.SwerveSubsystem;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -18,7 +20,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  
+  SwerveSubsystem drivetrain = new SwerveSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -40,7 +42,7 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    
+    drivetrain.setDefaultCommand(drivetrain.driveCommand(() -> -m_driverController.getLeftY(), () -> -m_driverController.getLeftX(), () -> -m_driverController.getRightX(), () -> false));
   }
 
   /**
--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -2,27 +2,44 @@ package frc.robot.subsystems;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import swervelib.SwerveDrive;
+import swervelib.math.SwerveMath;
 import swervelib.parser.SwerveParser;
 
-public class SwerveSubsystem {
-    public static double maximumSpeed = Units.feetToMeters(4.5);
+public class SwerveSubsystem extends SubsystemBase {
     private SwerveDrive swerveDrive;
-
-    public SwerveSubsystem() throws IOException {
-        swerveDrive = new SwerveParser(new File(Filesystem.getDeployDirectory(),"swerve")).createSwerveDrive(maximumSpeed);
+    // TODO!: adjust gear ratio in maximum speed calculation and in swerve config files
+    public static double maximumSpeed = 5676 / 6.75 * Math.PI * Units.inchesToMeters(4) / 60;
+    public SwerveSubsystem() {
+        try {
+            swerveDrive = new SwerveParser(new File(Filesystem.getDeployDirectory(),"swerve")).createSwerveDrive(maximumSpeed);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to initialize swerve drive", e);
+        }
     }
 
-    public Command driveCommand(DoubleSupplier translationXSupplier, DoubleSupplier translationYSupplier, DoubleSupplier headingXSupplier, DoubleSupplier headingYSupplier) {
+    // public Command driveCommand(DoubleSupplier translationXSupplier, DoubleSupplier translationYSupplier, DoubleSupplier headingXSupplier, DoubleSupplier headingYSupplier) {
+    //     return new Command() {
+    //         @Override
+    //         public void initialize() {
+    //             swerveDrive.driveFieldOriented(swerveDrive.swerveController.getTargetSpeeds(translationXSupplier.getAsDouble(), translationYSupplier.getAsDouble(), headingXSupplier.getAsDouble(), headingYSupplier.getAsDouble(), swerveDrive.getOdometryHeading().getRadians(), maximumSpeed));
+    //         }
+    //     };
+    // }
+
+    public Command driveCommand(DoubleSupplier translationXSupplier, DoubleSupplier translationYSupplier, DoubleSupplier headingSupplier, BooleanSupplier fieldOriented) {
         return new Command() {
             @Override
-            public void initialize() {
-                swerveDrive.driveFieldOriented(swerveDrive.swerveController.getTargetSpeeds(translationXSupplier.getAsDouble() * 0.8, translationYSupplier.getAsDouble() * 0.8, headingXSupplier.getAsDouble(), headingYSupplier.getAsDouble(), swerveDrive.getOdometryHeading().getRadians(), maximumSpeed));
+            public void execute() {
+                swerveDrive.drive(new Translation2d(translationXSupplier.getAsDouble() * swerveDrive.getMaximumChassisVelocity(), translationYSupplier.getAsDouble() * swerveDrive.getMaximumChassisVelocity()), headingSupplier.getAsDouble() * swerveDrive.getMaximumChassisAngularVelocity(), fieldOriented.getAsBoolean(), false);
             }
         };
     }
