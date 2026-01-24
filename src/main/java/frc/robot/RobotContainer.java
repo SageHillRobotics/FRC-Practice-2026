b850b65417d4e455b2c50645ec61cc6e34b3f5c4
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2026-01-24 11:37:44 -0800

    kitbot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,13 +7,16 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.SwerveModule;
 import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.YAGSLSwerveSubsystem;
+import frc.robot.subsystems.KitBot;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
-    SwerveSubsystem drivetrain = new SwerveSubsystem();
+    YAGSLSwerveSubsystem drivetrain = new YAGSLSwerveSubsystem();
+    KitBot kitbot = new KitBot();
 
     private final CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
@@ -22,7 +25,10 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
-        drivetrain.setDefaultCommand(drivetrain.drive(() -> MathUtil.applyDeadband(-m_driverController.getLeftY(), 0.1) * SwerveModule.MAX_SPEED, () -> MathUtil.applyDeadband(-m_driverController.getLeftX(), 0.1) * SwerveModule.MAX_SPEED, () -> MathUtil.applyDeadband(-m_driverController.getRightX(), 0.1) * SwerveModule.MAX_ANGULAR_RATE));
+        drivetrain.setDefaultCommand(drivetrain.driveCommand(() -> MathUtil.applyDeadband(-m_driverController.getLeftY(), 0.1) * YAGSLSwerveSubsystem.maximumSpeed, () -> MathUtil.applyDeadband(-m_driverController.getLeftX(), 0.1) * YAGSLSwerveSubsystem.maximumSpeed, () -> MathUtil.applyDeadband(-m_driverController.getRightX(), 0.1) * Math.PI / 2, () -> false));
+        m_driverController.a().whileTrue(kitbot.shoot());
+        m_driverController.b().whileTrue(kitbot.intake());
+        m_driverController.x().whileTrue(kitbot.eject());
     }
 
     public Command getAutonomousCommand() {
---
+++ src/main/java/frc/robot/subsystems/KitBot.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class KitBot extends SubsystemBase {
+    private SparkMax motor1;
+    private SparkMax motor2;
+
+    public KitBot() {
+        motor1 = new SparkMax(14, SparkMax.MotorType.kBrushed);
+        motor2 = new SparkMax(15, SparkMax.MotorType.kBrushed);
+    }
+
+    public Command intake() {
+        return Commands.run(() -> {
+            motor1.set(-1);
+            motor2.set(-1);
+        });
+    }
+
+    public Command shoot() {
+        return Commands.run(() -> {
+            motor1.set(-1);
+            motor2.set(1);
+        });
+    }
+
+    public Command eject() {
+        return Commands.run(() -> {
+            motor1.set(1);
+            motor2.set(1);
+        });
+    }
+}
